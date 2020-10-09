2400cf3b1cdfa0b9ebff35b79855b01adcc0419f
Vafin Artem <a.vafin85@gmail.com>
2020-10-09 21:40:38 +0500

    добавил модуль pin и card

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 "use strict";
 
-const FEATURES_CLASS_MAP = {
-  wifi: `popup__feature--wifi`,
-  dishwasher: `popup__feature--dishwasher`,
-  parking: `popup__feature--parking`,
-  washer: `popup__feature--washer`,
-  elevator: `popup__feature--elevator`,
-  conditioner: `popup__feature--conditioner`
-};
+
 const ROOMS_FOR_GUESTS_MAP = {
   1: [`1`],
   2: [`1`, `2`],
@@ -29,18 +22,11 @@ const TitleLength = {
   MIN: 30,
   MAX: 100
 };
-const HOUSE_TYPES = {
-  flat: `Квартира`,
-  bungalow: `Бунгало`,
-  palace: `Замок`,
-  house: `Дом`
-};
 
-const mapPinMain = window.map.mapNode.querySelector(`.map__pin--main`);
-const mapFiltersNode = window.map.mapNode.querySelector(`.map__filters-container`);
+const mapPinMain = window.pin.mapNode.querySelector(`.map__pin--main`);
+const mapFiltersNode = window.pin.mapNode.querySelector(`.map__filters-container`);
 const formFiltersNode = mapFiltersNode.querySelector(`.map__filters`);
-const mapPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-const mapCardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+
 const formNode = document.querySelector(`.ad-form`);
 
 
@@ -52,91 +38,8 @@ const getMainMapPinCoordinateY = () => {
   return parseInt(mapPinMain.style.top, 10) + (MainPinSize.HEIGHT);
 };
 
-
-const createPin = (obj) => {
-  const pinElement = mapPinTemplate.cloneNode(true);
-  pinElement.style.left = `${obj.location.x}px`;
-  pinElement.style.top = `${obj.location.y}px`;
-  pinElement.querySelector(`img`).src = obj.author.avatar;
-  pinElement.querySelector(`img`).alt = obj.offer.title;
-
-  return pinElement;
-};
-
-const createCard = (dataObject) => {
-  const cardElement = mapCardTemplate.cloneNode(true);
-  if (dataObject.offer.title.length) {
-    cardElement.querySelector(`.popup__title`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__title`).textContent = dataObject.offer.title;
-  }
-  if (dataObject.offer.address.length) {
-    cardElement.querySelector(`.popup__text--address`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__text--address`).textContent = dataObject.offer.address;
-  }
-  if (dataObject.offer.price) {
-    cardElement.querySelector(`.popup__text--price`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__text--price`).textContent = `${new Intl.NumberFormat(`ru-RU`).format(dataObject.offer.price)} ₽/ночь`;
-  }
-  if (dataObject.offer.type.length) {
-    cardElement.querySelector(`.popup__type`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__type`).textContent = HOUSE_TYPES[dataObject.offer.type];
-  }
-  cardElement.querySelector(`.popup__text--capacity`).textContent = `${dataObject.offer.rooms} ${window.util.getDeclension(dataObject.offer.rooms, [`комната`, `комнаты`, `комнат`])} ${dataObject.offer.guests > 0 ? `для ${dataObject.offer.guests} ${window.util.getDeclension(dataObject.offer.guests, [`гостя`, `гостей`, `гостей`])}` : `не для гостей`}`;
-  cardElement.querySelector(`.popup__text--time`).textContent = `Заезд после ${dataObject.offer.checkin}, выезд до ${dataObject.offer.checkout} `;
-
-  if (dataObject.offer.description.length) {
-    cardElement.querySelector(`.popup__description`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__description`).textContent = dataObject.offer.description;
-  }
-  if (dataObject.author.avatar.length) {
-    cardElement.querySelector(`.popup__avatar`).classList.remove(`hidden`);
-    cardElement.querySelector(`.popup__avatar`).src = dataObject.author.avatar;
-  }
-  if (dataObject.offer.features.length) {
-    cardElement.querySelector(`.popup__features`).classList.remove(`hidden`);
-    const featureNodes = cardElement.querySelectorAll(`.popup__feature`);
-    for (let i = 0; i < featureNodes.length; i++) {
-      for (let j = 0; j < dataObject.offer.features.length; j++) {
-        if (featureNodes[i].classList.contains(FEATURES_CLASS_MAP[dataObject.offer.features[j]])) {
-          featureNodes[i].classList.remove(`hidden`);
-          break;
-        }
-      }
-    }
-  }
-  if (dataObject.offer.photos.length) {
-    cardElement.querySelector(`.popup__photos`).classList.remove(`hidden`);
-    let photoNode = cardElement.querySelector(`.popup__photo`);
-    photoNode.src = dataObject.offer.photos[0];
-    if (dataObject.offer.photos.length > 1) {
-      const fragment = document.createDocumentFragment();
-      for (let i = 1; i < dataObject.offer.photos.length; i++) {
-        fragment.appendChild(photoNode.cloneNode(true)).src = dataObject.offer.photos[i];
-      }
-      photoNode.parentElement.appendChild(fragment);
-    }
-  }
-
-  return cardElement;
-};
-
-const createPinsNodeFragment = (pinsArr) => {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < pinsArr.length; i++) {
-    fragment.appendChild(createPin(pinsArr[i]));
-  }
-
-  return fragment;
-};
-
-const createСardFragment = (cardObj) => {
-  const fragment = document.createDocumentFragment();
-  fragment.appendChild(createCard(cardObj));
-  return fragment;
-};
-
 const initPinsScreen = () => {
-  const pinsNodesFragment = createPinsNodeFragment(window.data.pinsDataArray);
+  const pinsNodesFragment = window.pin.createPinsNodeFragment(window.data.pinsDataArray);
   window.pin.mapPinsNode.appendChild(pinsNodesFragment);
 };
 
@@ -211,7 +114,7 @@ const toggleDisabledOnFormNodes = () => {
 };
 
 const onActiveMode = () => {
-  window.map.mapNode.classList.remove(`map--faded`);
+  window.pin.mapNode.classList.remove(`map--faded`);
   formNode.classList.remove(`ad-form--disabled`);
   toggleDisabledOnFormNodes();
 };
@@ -241,13 +144,13 @@ mapPinMain.addEventListener(`click`, function () {
 
   pinsArr.forEach((element, index) => {
     element.addEventListener(`click`, () => {
-      cardNode = window.map.mapNode.querySelector(`.map__card`);
+      cardNode = window.pin.mapNode.querySelector(`.map__card`);
       if (cardNode) {
         removeActiveCard();
       }
-      const cardNodesFragment = createСardFragment(window.data.pinsDataArray[index]);
-      window.map.mapNode.insertBefore(cardNodesFragment, mapFiltersNode);
-      cardNode = window.map.mapNode.querySelector(`.map__card`);
+      const cardNodesFragment = window.card.createСardFragment(window.data.pinsDataArray[index]);
+      window.pin.mapNode.insertBefore(cardNodesFragment, mapFiltersNode);
+      cardNode = window.pin.mapNode.querySelector(`.map__card`);
       const closeButton = cardNode.querySelector(`.popup__close`);
       closeButton.addEventListener(`click`, removeActiveCard);
       document.addEventListener(`keydown`, onPopupEscPress);

b92125beb0e297dadb11c05c8ceb5256185ccab2
Vafin Artem <a.vafin85@gmail.com>
2020-10-26 21:37:24 +0500

    Правка добавления изображения комнаты

--- bundle.js
+++ bundle.js
@@ -728,7 +728,7 @@ const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
 const avatarImageChooserNode = window.form.formNode.querySelector(`.ad-form-header__input`);
 const previewAvatarNode = window.form.formNode.querySelector(`.ad-form-header__preview img`);
 const roomImageChooserNode = window.form.formNode.querySelector(`.ad-form__input`);
-const previewRoomImageNode = window.form.formNode.querySelector(`.ad-form__photo`);
+const previewRoomNode = window.form.formNode.querySelector(`.ad-form__photo img`);
 
 
 const addImage = (imageChooserInput, previewImageElement) => {
@@ -742,23 +742,14 @@ const addImage = (imageChooserInput, previewImageElement) => {
   if (matches) {
     let reader = new FileReader();
 
-    if (imageChooserInput === roomImageChooserNode) {
-      reader.addEventListener(`load`, () => {
-        let previewRoomImageElement = false;
-        if (previewRoomImageElement) {
-          const previewRoomImage = document.createElement(`img`);
-          previewRoomImage.classList.add(`ad-form__photo-img`);
-          previewRoomImageElement = previewImageElement.appendChild(previewRoomImage);
-          previewRoomImageElement.src = reader.result;
-        } else {
-          window.backend.showError(`Вы можете загрузить только одно фото`);
-        }
-      });
-    } else {
-      reader.addEventListener(`load`, () => {
+    reader.addEventListener(`load`, () => {
+      if (previewImageElement.classList.contains(`hidden`)) {
+        previewImageElement.classList.remove(`hidden`);
         previewImageElement.src = reader.result;
-      });
-    }
+      } else {
+        previewImageElement.src = reader.result;
+      }
+    });
 
     reader.readAsDataURL(image);
   }
@@ -769,7 +760,7 @@ avatarImageChooserNode.addEventListener(`change`, () => {
 });
 
 roomImageChooserNode.addEventListener(`change`, () => {
-  addImage(roomImageChooserNode, previewRoomImageNode);
+  addImage(roomImageChooserNode, previewRoomNode);
 });
 
 })();
